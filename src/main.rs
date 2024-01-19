6db8238c3aa43828983056e65711f52791ddebc9
Merveille van Eck <merveillevaneck@gmail.com>
2024-01-20 00:11:25 +0200

    added alert processing and hardcoded vpn address of host server
    notes: the vpn host server domain is inaccessible to the outside world

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+use rodio::source::{SineWave, Source};
+use rodio::{OutputStream, Sink};
 use rust_socketio::{Payload, SocketBuilder};
 use serde_json::json;
-use std::io::*;
+use std::time::Duration;
+use std::{io::*, os::unix::process};
 
 enum Instruction {
     Alert,
@@ -12,7 +15,7 @@ impl Instruction {
         match instruction {
             "alert" => Instruction::Alert,
             "print" => Instruction::Print,
-            _ => panic!("Unknown instruction"),
+            _ => panic!("unknown instruction"),
         }
     }
     fn process(instruction: Instruction, message: Option<&str>) {
@@ -32,20 +35,35 @@ fn print(message: &str) {
 
 fn alert() {
     println!("Alert!");
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    let wrapped_sink = Sink::try_new(&stream_handle);
+
+    // Add a dummy source of the sake of the example.
+    let source = SineWave::new(440.0)
+        .take_duration(Duration::from_secs_f32(0.25))
+        .amplify(0.20);
+    let sink = wrapped_sink.unwrap();
+    sink.append(source);
+    sink.sleep_until_end();
 }
 
 fn process_instruction(instruction: &str) {
-    let code = Instruction::decode(instruction);
-    Instruction::process(code, None);
+    let decoded = Instruction::decode(instruction);
+    Instruction::process(decoded, None);
 }
 
 fn main() {
     let callback = |payload: Payload, _| match payload {
-        Payload::String(str) => process_instruction(&str),
+        Payload::String(str) => {
+            // let message = str[1..str.len() - 1].to_string();
+            // process_instruction(&message);
+            alert();
+        }
         Payload::Binary(bin_data) => println!("{:?}", bin_data),
     };
 
-    let host = "127.0.0.1:3001";
+    let host = "https://pop-os.tail4f070.ts.net";
 
     let mut socket = SocketBuilder::new(host)
         .on("message", callback)
